64f3e4bcd9c09ef9686bff96ff7097d76666d179
Mubashirkhank <82580638+Mubashirkhank@users.noreply.github.com>
2025-09-07 02:24:37 -0400

    Add .next/static/chunks/src_app_redesigned-home_page_tsx_ddd7e2e8._.js 1757226278235

---
+++ .next/static/chunks/src_app_redesigned-home_page_tsx_ddd7e2e8._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_3691ab6e._.js",
+  "static/chunks/src_components_RedesignedHomepage_tsx_a828fa4d._.js"
+],
+    source: "dynamic"
+});
